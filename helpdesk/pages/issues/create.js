36ee0ab7155e67f5a9b968935a3ca67c82c08e96
Ranum99 <aleksanderranum99@hotmail.com>
2021-11-08 14:31:46 +0100

    Egne sider + navigation
    Egne sider for ny henvendelse, alle henvendelser, og en enkelt henvendelse. + en navigasjon

---
+++ helpdesk/pages/issues/create.js
@@ -0,0 +1,12 @@
+import Navigation from '@/components/navigation'
+import SupportForm from '@/components/SupportForm'
+
+export default function Home() {
+  return (
+    <main>
+      <Navigation />
+      <SupportForm />
+      />
+    </main>
+  )
+}
