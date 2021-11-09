54460430299c319f20249069a24e36e55b8ceb75
ranum99 <aleksanderranum99@hotmail.com>
2021-11-09 18:58:37 +0100

    Mappestruktur (MVC) til comments og issues

---
+++ helpdesk/pages/api/comments/[id].js
@@ -0,0 +1,16 @@
+import * as commentsController from '@/features/comments/comments.controller'
+
+export default async function handler(req, res) {
+  const { method } = req
+
+  switch(method?.toLowerCase()) {
+    case 'post':
+      await commentsController.createComment(req, res);
+      break;
+    case 'get':
+      await commentsController.listComments(req, res);
+      break;
+    default: 
+      res.status(405).end();
+  }
+}
