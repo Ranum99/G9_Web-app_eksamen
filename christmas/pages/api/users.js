09f7ff368498d938f35e6508cf3d3d23899dc361
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2021-11-13 17:47:21 +0100

    small update in controller for users

--- christmas/pages/api/users.js
+++ christmas/pages/api/users.js
@@ -1,15 +1,11 @@
 import prisma from '@/lib/clients/db'
-import * as UserController from "@/features/users/user.controller"
+import * as UserController from "@/features/users/users.controller"
 
 export default async function handler(req, res) {
   switch (req.method.toLowerCase()) {
 
     case 'get': {
-      const users = await prisma.user.findMany()
-
-      return res.status(200).json({ success: true, users })
-
-      await UserController.getUsers(req, req)
+           await UserController.getUsers(req, req)
     }
     
     default:
