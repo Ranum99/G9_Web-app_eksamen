a617d1691b6a9c555ecd0ee45215ebb21e68c312
Jonathan <jreberg98@gmail.com>
2021-11-16 23:25:00 +0100

    Viser når luka åpner, og dagens kode

--- christmas/components/CalendarCard.js
+++ christmas/components/CalendarCard.js
@@ -4,15 +4,14 @@ const CalendarCard = ({ number, code, openAt }) => {
   const today = new Date()
   // Setter klokkeslett til 0, skal bare sammenligne dato
   today.setHours(0, 0, 0, 0)
-
   const slotDay = new Date(openAt)
   // Samme som over, skal bare sammenligne dato
   slotDay.setHours(0, 0, 0, 0)
 
-  console.log('\n\n\n')
-  console.log(slotDay)
-  console.log(today)
-  console.log(today)
+  const getCoupon = () => {
+    // TODO: Hente/generere coupon fra DB
+    return '1234abcd'
+  }
 
   if (today.getTime() === slotDay.getTime()) {
     type = 'active'
@@ -24,7 +23,10 @@ const CalendarCard = ({ number, code, openAt }) => {
   }
   return (
     <article className=".center" className={type}>
-      {openAt}
+      <h2>{number}</h2>
+      {type == 'notAvalibalie' &&
+        'Åpner om ' + (slotDay - today) / 1000 / 60 / 60 / 24 + ' dager'}
+      {type == 'active' && getCoupon()}
     </article>
   )
 }
