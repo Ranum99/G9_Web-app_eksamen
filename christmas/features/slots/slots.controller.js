d6a1d4a0f94271b3c92568f0c83a7f56bc210901
Jonathan <jreberg98@gmail.com>
2021-11-18 10:07:24 +0100

    Bugfix

--- christmas/features/slots/slots.controller.js
+++ christmas/features/slots/slots.controller.js
@@ -1,28 +1,19 @@
-
 import * as slotService from './slots.service'
 
 import { ApiResponse } from '@/lib/api/apiResponse'
 
-
-
-
 export const getSlots = async (req, res, id) => {
-
   if (!Number(id)) {
-
-    return ApiResponse(res).badRequest(`${id} er ikke et tall` )
+    return ApiResponse(res).badRequest(`${id} er ikke et tall`)
   }
-
 
   const slots = await slotService.checkIfExsist(id)
 
   if (!slots?.success) {
-    return ApiResponse(res).badRequest("Error getting data")
-  } 
-   return ApiResponse(res).ok(slots)
-  
+    return ApiResponse(res).badRequest('Error getting data')
+  }
+  return ApiResponse(res).ok(slots.data)
 }
-
 
 /*
 export const getSlots = async (req, res, id) => {
