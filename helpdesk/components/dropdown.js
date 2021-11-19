82c93a34eadec7fac767cbf081b802dc4f5116bd
ranum99 <aleksanderranum99@hotmail.com>
2021-11-19 16:44:39 +0100

    Update dropdown.js

--- helpdesk/components/dropdown.js
+++ helpdesk/components/dropdown.js
@@ -1,7 +1,7 @@
 export default function Dropdown({ name, handleInputOnChange, options }) {
 
   return (
-    <select name={name} onChange={handleInputOnChange}>
+    <select required={true} name={name} onChange={handleInputOnChange}>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.name}
