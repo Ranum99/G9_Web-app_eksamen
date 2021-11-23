a9e4b51a64f9158db5fe08f433f057a59392746f
Jonathan <jreberg98@gmail.com>
2021-11-23 09:07:17 +0100

    Kan henten inn slots igjen

--- christmas/features/slots/slots.controller.js
+++ christmas/features/slots/slots.controller.js
@@ -2,42 +2,15 @@ import * as slotService from './slots.service'
 
 import { ApiResponse } from '@/lib/api/apiResponse'
 
-
-
-
 export const getSlots = async (req, res, id) => {
-
   if (!Number(id)) {
-
-    return ApiResponse(res).badRequest(`${id} er ikke et tall` )
+    return ApiResponse(res).badRequest(`${id} er ikke et tall`)
   }
-
 
   const slots = await slotService.checkIfExsist(id)
 
   if (!slots?.success) {
-    return ApiResponse(res).badRequest("Error getting data")
-  } 
-   return ApiResponse(res).ok(slots)
-  
-}
-
-
-/*
-export const getSlots = async (req, res, id) => {
-  if (!Number(id)) {
-    return res
-      .status(400)y
-      .json({ success: false, error: `${id} er ikke et tall` })
+    return ApiResponse(res).badRequest('Error getting data')
   }
-
-  const slots = await slotService.checkIfExsist(id)
-
-  if (slots.success) {
-    res.status(200).json(slots)
-  } else {
-    // TODO: feilkode
-    res.status(400).json(slots)
-  }
+  return ApiResponse(res).ok(slots.data)
 }
-*/
