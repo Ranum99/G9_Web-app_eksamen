25a16964e9c05637a319dd845567728bdb2fcc21
CrazyCammis <62859438+CrazyCammis@users.noreply.github.com>
2021-11-26 14:21:29 +0100

    started userslot controller test

--- christmas/__tests__/userSlot/userSlot.controller.test.js
+++ christmas/__tests__/userSlot/userSlot.controller.test.js
@@ -1,66 +1,12 @@
-import { createdUserSlot  } from '@/features/userSlot/userSlot.service'
+import httpMocks from 'node-mocks-http'
 
-import axios from 'axios'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { getUserSlot} from '@/features/userSlot/userSlot.controller'
 import prisma from '@/lib/clients/db'
-import { create } from '@/features/userSlot/userSlot.repository'
-const url = 'http://localhost:3000/api/userSlot'
 
+const url = 'http://localhost:3000/api/users'
 
-
-
-describe("create  Slot", () =>{
-  const server = setupServer()
-
-
-  beforeEach(async() => {
-    await prisma.userSlot.deleteMany({})
-  })
-
-  beforeAll(() => {s
-  server.listen() 
-  })
-
-  afterEach(() => server.resetHandlers())
-  afterAll(() => {
-    server.close()
-  })
-
-  describe("creating slots", () =>{
-    describe("when given slotid  and  userid", ()=>{
-      it("should respond with a true sucsess", async () => {
-        server.use(
-          rest.pos(url, async (req, res, ctx) =>{
-            const {slotId, userId}  =req.body
-
-            const data = await createdUserSlot({slotId, userId})
-            
-            return res(ctx.json(data))
-          })
-        )
-          const  response  = await axios.post(url, {slotId: 2, userId : 3})
-          expect(response.data.success).toBe(true)
-
-     })
-    })
-  })
-
-  describe("when given missing info", () =>{
-    it("should respond with false sucsess", async()=>{
-      server.use(
-        rest.post(url, async(req, res, ctx)=>{
-          const {slotId, userId} = req.body
-          
-
-          const data = await createdUserSlot({slotId, userId})
-
-          return res(ctx.json(data))
-        })
-      )
-      
-      const response = await axios.post(url)
-      expect (response.data.success).toBe( false)
-    })
+describe("sometihing", () =>{
+  beforeEach(async () => {
+    await prisma.UserSlot.deleteMany({})
   })
 })
