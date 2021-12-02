7f8f79535a8e905c964f16fb48c3cb63bae87860
Jonathan <jreberg98@gmail.com>
2021-12-02 13:40:38 +0100

    Kan delta / hente kode for tidligere slots

--- christmas/components/CalendarCard.js
+++ christmas/components/CalendarCard.js
@@ -1,9 +1,9 @@
 import { userInfo } from '@/lib/utils/user'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const CalendarCard = ({ number, openAt, id }) => {
   const [coupon, setCoupon] = useState('')
-  let type = ''
+  const [type, setType] = useState()
 
   const today = new Date()
   // Setter klokkeslett til 0, skal bare sammenligne dato
@@ -13,30 +13,33 @@ const CalendarCard = ({ number, openAt, id }) => {
   slotDay.setHours(0, 0, 0, 0)
 
   const getCoupon = async () => {
+    if (type == 'notAvalibalie') {
+      alert('Luken er ikke åpen enda')
+      return
+    }
+
     const user = await userInfo()
 
     const response = await fetch(
       '/api/userSlot?slotId=' + id + '&userId=' + user.user.id
     )
     const data = await response.json()
-
+    setType('opened')
     setCoupon(data.data.coupon)
   }
 
-  if (today.getTime() === slotDay.getTime()) {
-    type = 'active'
-    getCoupon()
-  } else if (today < slotDay) {
-    type = 'notAvalibalie'
-  } else {
-    type = ''
-  }
+  useEffect(() => {
+    if (today < slotDay) {
+      setType('notAvalibalie')
+    }
+  }, [])
+
   return (
-    <article className=".center" className={type}>
+    <article className=".center" className={type} onClick={() => getCoupon()}>
       <h2>{number}</h2>
       {type == 'notAvalibalie' &&
         'Åpner om ' + (slotDay - today) / 1000 / 60 / 60 / 24 + ' dager'}
-      {type == 'active' && coupon}
+      {coupon}
     </article>
   )
 }
