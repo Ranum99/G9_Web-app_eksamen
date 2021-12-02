5cecfd76951596da3692b609fc069399390c0cfc
Jonathan <jreberg98@gmail.com>
2021-12-02 11:58:31 +0100

    Endra navn fra admin til dashboard

--- christmas/pages/dashboard.js
+++ christmas/pages/dashboard.js
@@ -2,33 +2,6 @@ import Slot from '@/components/Slot'
 import SlotList from '@/components/SlotList'
 import { useEffect, useState } from 'react'
 
-const data = [
-  {
-    id: 0,
-    name: 'Julekalender',
-    createdAt: Date.now(),
-    // Lukene i en kalender
-    slot: [
-      {
-        id: 25986,
-        slug: 'abcd1234',
-        order: 1,
-        createdAt: new Date(),
-        openAt: new Date(),
-      },
-      {
-        id: 23845,
-        slug: 'dust3245',
-        order: 2,
-        createdAt: new Date(),
-        openAt: new Date(),
-      },
-    ],
-  },
-]
-
-const julekalender = data[0]
-
 const admin = () => {
   const [calendar, setCalendar] = useState({
     id: 0,
