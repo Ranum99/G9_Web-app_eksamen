2a6c7e21f0f3368842e33b3201ca1343433d0ce3
Jonathan <jreberg98@gmail.com>
2021-12-06 14:54:42 +0100

    Tester feilmeldinger

--- christmas/__tests__/userSlot/userSlot.service.test.js
+++ christmas/__tests__/userSlot/userSlot.service.test.js
@@ -1,3 +1,5 @@
+// I testene brukes post istedenfor get, selv om APIet bruker get forespørlser. Grunnen til det er at det dermed blir mer oversiktlig å legge legge til data i forespørslene
+
 import { getUserSlot } from '@/features/userSlot/userSlot.service'
 
 import axios from 'axios'
@@ -26,7 +28,6 @@ describe('create UserSlot', () => {
   describe('when given slotid and userid', () => {
     it('should respond with a true success', async () => {
       server.use(
-        // Skal egentlig være get, men da hentes ikke dataen ut av body
         rest.post(url, async (req, res, ctx) => {
           const { slotId, userId } = req.body
 
@@ -35,15 +36,14 @@ describe('create UserSlot', () => {
           return res(ctx.json(data))
         })
       )
-      // Skal egentlig være get, men da hentes ikke dataen ut av body
       const response = await axios.post(url, { slotId: 73, userId: 33 })
 
       expect(response.data.success).toBe(true)
     })
   })
 
-  describe('when ID is wrong', () => {
-    it('wrong slotId should give success = false', async () => {
+  describe('when slotID is wrong', () => {
+    it(' success = false', async () => {
       server.use(
         rest.post(url, async (req, res, ctx) => {
           const { slotId, userId } = req.body
@@ -57,6 +57,74 @@ describe('create UserSlot', () => {
       const response = await axios.post(url, { slotId: 0, userId: 33 })
 
       expect(response.data.success).toBe(false)
+    })
+
+    it('type=Slot.NotFound and error:"Slot ikke funnet', async () => {
+      server.use(
+        rest.post(url, async (req, res, ctx) => {
+          const { slotId, userId } = req.body
+
+          const data = await getUserSlot(slotId, userId)
+
+          return res(ctx.json(data))
+        })
+      )
+
+      const response = await axios.post(url, { slotId: 0, userId: 33 })
+
+      expect(response.data.type).toBe('Slot.NotFound')
+      expect(response.data.error).toBe('Slot ikke funnet')
+    })
+
+    it('wrong userId should give success = false', async () => {
+      server.use(
+        rest.post(url, async (req, res, ctx) => {
+          const { slotId, userId } = req.body
+
+          const data = await getUserSlot(slotId, userId)
+
+          return res(ctx.json(data))
+        })
+      )
+
+      const response = await axios.post(url, { slotId: 73, userId: 3 })
+
+      expect(response.data.success).toBe(false)
+    })
+  })
+
+  describe('when userId is wrong', () => {
+    it('success = false', async () => {
+      server.use(
+        rest.post(url, async (req, res, ctx) => {
+          const { slotId, userId } = req.body
+
+          const data = await getUserSlot(slotId, userId)
+
+          return res(ctx.json(data))
+        })
+      )
+
+      const response = await axios.post(url, { slotId: 75, userId: 3 })
+
+      expect(response.data.success).toBe(false)
+    })
+
+    it('type=User.NotFound and error:"User ikke funnet', async () => {
+      server.use(
+        rest.post(url, async (req, res, ctx) => {
+          const { slotId, userId } = req.body
+
+          const data = await getUserSlot(slotId, userId)
+
+          return res(ctx.json(data))
+        })
+      )
+
+      const response = await axios.post(url, { slotId: 75, userId: 3 })
+
+      expect(response.data.type).toBe('User.NotFound')
+      expect(response.data.error).toBe('Bruker ikke funnet')
     })
 
     it('wrong userId should give success = false', async () => {
